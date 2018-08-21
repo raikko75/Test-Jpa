e560c123e6034bddd955830b987ff76e3c5aa93e
test <robillardbastian@outlook.fr>
2018-08-21 12:01:35 +0200

    JPA

---
+++ src/main/java/TestJpa.java
@@ -0,0 +1,34 @@
+
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Cache;
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.FlushModeType;
+import javax.persistence.LockModeType;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnitUtil;
+import javax.persistence.Query;
+import javax.persistence.StoredProcedureQuery;
+import javax.persistence.SynchronizationType;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.CriteriaUpdate;
+import javax.persistence.metamodel.Metamodel;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory = new Persistence().createEntityManagerFactory("pu_essai");  //("pu_essai");
+		EntityManager entityManager = entityManagerFactory.createEntityManager(); 
+		
+		
+	}
+
+}
